de7a2dd69261ef813640b1d836834dbf3694f00e
Ehsan Tashakkori <ehsan.catalan@gmail.com>
2023-12-07 21:11:44 +0330

    add getList route

--- serverX.js
+++ serverX.js
@@ -2,6 +2,11 @@ const http = require("http");
 const qs = require("querystring");
 
 const serverX = http.createServer((req, res) => {
+  // Enable CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   if (req.method === "POST" && req.url === "/insert") {
     let body = "";
 
@@ -21,6 +26,55 @@ const serverX = http.createServer((req, res) => {
         <p>Password: ${formData.password}</p>
       `);
     });
+  } else if (req.method === "GET" && req.url === "/getList") {
+    const options = {
+      hostname: "localhost",
+      port: 8082,
+      path: "/getList",
+      method: "GET",
+    };
+
+    const reqY = http.request(options, (resY) => {
+      let responseData = "";
+
+      resY.on("data", (data) => (responseData += data));
+
+      resY.on("end", () => {
+        console.log("Response from Server Y:", responseData); // Log the response
+
+        if (resY.statusCode === 200) {
+          try {
+            const userList = JSON.parse(responseData);
+            res.writeHead(200, {
+              "Content-Type": "application/json",
+            });
+            res.end(JSON.stringify(userList));
+          } catch (error) {
+            console.error(
+              "Error parsing user list from Server Y:",
+              error.message
+            );
+            res.writeHead(500, { "Content-Type": "text/plain" });
+            res.end("Internal Server Error");
+          }
+        } else {
+          console.error(
+            "Error in response from Server Y. Status code:",
+            resY.statusCode
+          );
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Internal Server Error");
+        }
+      });
+    });
+
+    reqY.on("error", (error) => {
+      console.error(`Error making request to Server Y: ${error.message}`);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+    });
+
+    reqY.end();
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("Page Not Found!");
@@ -30,7 +84,7 @@ const serverX = http.createServer((req, res) => {
 const PORT_X = 8080;
 
 serverX.listen(PORT_X, () => {
-  console.log(`Server is running at http://localhost:${PORT_X}`);
+  console.log(`Server X is running at http://localhost:${PORT_X}`);
 });
 
 function sendDataToServerY(data) {
--- serverY.js
+++ serverY.js
@@ -2,6 +2,11 @@ const http = require("http");
 const fs = require("fs");
 
 const serverY = http.createServer((req, res) => {
+  // Enable CORS
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   if (req.method === "POST" && req.url === "/receive") {
     let body = "";
 
@@ -16,6 +21,21 @@ const serverY = http.createServer((req, res) => {
       res.writeHead(200, { "Content-Type": "text/plain" });
       res.end("Data received on Server Y!");
     });
+  } else if (req.method === "GET" && req.url === "/getList") {
+    try {
+      const jsonData = fs.readFileSync("data.json", "utf8");
+      const userList = JSON.parse(jsonData);
+
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      });
+      res.end(JSON.stringify(userList));
+    } catch (error) {
+      console.error("Error reading data from JSON file:", error.message);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.end("Internal Server Error");
+    }
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end("Page Not Found!");
